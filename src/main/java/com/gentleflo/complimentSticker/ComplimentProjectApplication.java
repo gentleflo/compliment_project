ece90554f8bf8e02df1443065ac8e29a0577f13e
202-07\User <gentleflo0106@gmail.com>
2021-10-06 16:47:44 +0900

    [이나은]  - 최초 파일 커밋

---
+++ src/main/java/com/gentleflo/complimentSticker/ComplimentProjectApplication.java
@@ -0,0 +1,13 @@
+package com.gentleflo.complimentSticker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplimentProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplimentProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gentleflo/ex/ComplimentProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gentleflo.ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplimentProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
