76ce4980726dd726eee7fe487727a5e241f5d978
202-07\User <gentleflo0106@gmail.com>
2021-11-05 14:05:28 +0900

    [이나은]  - 배포용 코드 수정

--- src/main/java/com/gentleflo/complimentSticker/ComplimentProjectApplication.java
+++ src/main/java/com/gentleflo/complimentSticker/ComplimentProjectApplication.java
@@ -2,12 +2,15 @@ package com.gentleflo.complimentSticker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ComplimentProjectApplication {
+public class ComplimentProjectApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ComplimentProjectApplication.class, args);
 	}
+	
+	
 
 }
